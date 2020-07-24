e5b8c547daeac94baefdbea29910f33a66f4ebb3
pratikkedar <45839850+pratikkedar@users.noreply.github.com>
2020-07-24 11:30:53 +0530

    star_triangle_without_space() is added

--- Patterns.cpp
+++ Patterns.cpp
@@ -10,7 +10,18 @@ class Patterns{
             n=val;
         }
     void star_triangle();
+    void star_triangle_without_space();
 };
+
+inline void Patterns::star_triangle_without_space(){
+    int n=this->n;
+    for(int i=0;i<n;i++){
+        for(int k=0;k<=i;k++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
 
 inline void Patterns::star_triangle(){
     int n=this->n;
@@ -32,6 +43,7 @@ int main(){
     cout<<"Enter the value"<<endl;
     cin>>val;
     Patterns obj(val);
-    obj.star_triangle();
+    //obj.star_triangle();
+    obj.star_triangle_without_space();
     return 0;
 }
